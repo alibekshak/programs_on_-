d332ecffd5ae49004cda28b4d77b237c899adc5e
alibekshak <alibekshakiro1@gmail.com>
2023-04-07 15:02:15 +0600

    first programs

---
+++ lesson2.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string name = get_string("What is your name? ");  // строки всегда записываются с двойными кавычками
+    printf("Hellow, %s\n", name); // printing 
+    return 0; // запершает функцию main 
+}
